a00049c28650e7f686f7823e9fd439c7f764a61b
aialvi <aminulialvi@gmail.com>
2019-07-28 00:56:35 +0600

    Setup Activity Fixed

--- app/src/main/java/com/optimizingknowledge/ok/SetupActivity.java
+++ app/src/main/java/com/optimizingknowledge/ok/SetupActivity.java
@@ -1,28 +1,24 @@
 package com.optimizingknowledge.ok;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+
 import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
+
 
 
 import java.util.HashMap;
@@ -36,11 +32,10 @@ public class SetupActivity extends AppCompatActivity
     private CircleImageView ProfileImage;
     private FirebaseAuth mAuth;
     private DatabaseReference UsersRef;
-    private StorageReference UserProfileImageRef;
+
 
 
     String currentUserID;
-    final static int Gallery_Pick = 1;
 
 
     @Override
@@ -52,7 +47,7 @@ public class SetupActivity extends AppCompatActivity
         mAuth = FirebaseAuth.getInstance();
         currentUserID = mAuth.getCurrentUser().getUid();
         UsersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID);
-        UserProfileImageRef = FirebaseStorage.getInstance().getReference().child("profile images");
+
 
         IdNumber = (EditText) findViewById(R.id.setup_id_number);
         FullName = (EditText) findViewById(R.id.setup_full_name);
@@ -60,99 +55,16 @@ public class SetupActivity extends AppCompatActivity
         SaveInformationbuttion = (Button) findViewById(R.id.setup_information_button);
         ProfileImage = (CircleImageView) findViewById(R.id.setup_profile_image);
 
-    SaveInformationbuttion.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            SaveAccountSetupInformation();
-        }
-    });
-
-    // Gallery access
-    ProfileImage.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent galleryIntent = new Intent();
-            galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-            galleryIntent.setType("image/*");
-            startActivityForResult(galleryIntent, Gallery_Pick);
-
-        }
-    });
-
-
-
+        SaveInformationbuttion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SaveAccountSetupInformation();
+            }
+        });
     }
 
 
-// image starts
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode==Gallery_Pick && resultCode==RESULT_OK && data!=null)
-        {
-            Uri ImageUri = data.getData();
-
-            CropImage.activity()
-                    .setGuidelines(CropImageView.Guidelines.ON)
-                    .setAspectRatio(1, 1)
-                    .start(this);
-        }
-
-        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
-        {
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-
-            if(resultCode == RESULT_OK)
-            {
-
-                Uri resultUri = result.getUri();
-
-                StorageReference filePath = UserProfileImageRef.child(currentUserID + ".jpg");
-
-                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull final Task<UploadTask.TaskSnapshot> task)
-                    {
-                        if(task.isSuccessful())
-                        {
-                            Toast.makeText(SetupActivity.this, "Profile Image stored successfully to Firebase storage...", Toast.LENGTH_SHORT).show();
-
-                            final String downloadUrl = task.getResult().toString();
-
-                            UsersRef.child("profileimage").setValue(downloadUrl)
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task)
-                                        {
-                                            if(task.isSuccessful())
-                                            {
-                                                Intent selfIntent = new Intent(SetupActivity.this, SetupActivity.class);
-                                                startActivity(selfIntent);
-
-                                                Toast.makeText(SetupActivity.this, "Profile Image stored to Firebase Database Successfully...", Toast.LENGTH_SHORT).show();
-
-                                            }
-                                            else
-                                            {
-                                                String message = task.getException().getMessage();
-                                                Toast.makeText(SetupActivity.this, "Error Occured: " + message, Toast.LENGTH_SHORT).show();
-
-                                            }
-                                        }
-                                    });
-                        }
-                    }
-                });
-            }
-            else
-            {
-                Toast.makeText(this, "Error Occured: Image can not be cropped. Try Again.", Toast.LENGTH_SHORT).show();
-
-            }
-        }
-    }
     //image ends
 
     // Setting Account Function
